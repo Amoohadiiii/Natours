68708cee405fae7f8848d2e36a653625414b3684
hadipouranian <hadi.pouranian@gmail.com>
2021-08-20 15:45:01 +0800

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
@@ -29,6 +30,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 1) GLOBAL Middlewares
+// Implement CORS
+// Access-Contrlo-Allow-Origin
+app.use(cors());
+
+app.options('*', cors());
+
 // Set security HTTP headers
 // app.use(helmet());
 app.use(
